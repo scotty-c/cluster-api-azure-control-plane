2863420049d646f872ac1eec973eb1c7c2353528
scott coulton <scott.coulton@microsoft.com>
2021-03-15 09:53:40 +1100

    first commit

---
+++ cloud-init.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "# make..."
+sudo apt-get install -y \
+        make
+
+echo "# microk8s..."
+sudo snap install microk8s --classic --channel=1.19
+mkdir -p $HOME/.kube/
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+sudo microk8s config > $HOME/.kube/config
+sudo microk8s enable helm3
+
+tee -a ~/.bash_aliases <<'EOF'
+function helm {
+        sudo microk8s helm3 "$@"
+}
+
+function kubectl {
+        sudo microk8s kubectl "$@"
+}
+EOF
+source ~/.bash_aliases
+
+echo "# k9s..."
+VERSION='0.24.2'
+curl -OL https://github.com/derailed/k9s/releases/download/v${VERSION}/k9s_Linux_x86_64.tar.gz
+mkdir -p tmp/
+tar -C tmp/ -xvf k9s_Linux_x86_64.tar.gz
+rm k9s_Linux_x86_64.tar.gz
+sudo mv -f tmp/k9s /usr/local/bin
+rm -rf tmp/
+mkdir -p $HOME/.k9s/
+
+echo "# az cli..."
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+echo "# complete!"
